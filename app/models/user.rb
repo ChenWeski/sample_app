0134ebc14476603915374ce849575f1e755260c8
Chen Golbraikh <chen@goweski.com>
2018-05-20 11:48:06 +0300

    signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 	before_save {self.email = email.downcase}
 	validates :name, presence: true, length: {maximum: 9}
 	validates :email, presence: true, length: {maximum: 15}, format: {with: /[@]/}, uniqueness: {case_sensitive: false}
-	
+	   
 	has_secure_password
 	validates :password, presence: true, length:{minimum: 5}	
 end
